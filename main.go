8f134733664aae7357ba4ac1d4b693c7905bc5c2
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2021-11-06 22:51:27 +0800

    commit 2

--- main.go
+++ main.go
@@ -1,1 +1,53 @@
-package forum
+package main
+
+import (
+	"forum/dao"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+//帖子
+type Article struct {
+	Id  int
+	Context  string
+	Author  string
+	Replies   []Reply//所有回复
+
+}
+
+//帖子下面的回复
+type Reply struct{
+	Id int
+	Author  string
+	Context string
+	//用来存帖子下面回复的回复
+	Comments  []Comment
+}
+
+//帖子下面回复的回复
+type Comment struct {
+	Author  string
+	Context string
+}
+type User struct {
+	Id   int
+	Name  string  //用户名
+   	Pwd   string
+
+}
+
+func main() {
+	// 连接数据库
+	err := dao.InitMySQL()
+	if err != nil {
+		panic(err)
+	}
+	defer dao.Close()  // 程序退出关闭数据库连接
+	r:=gin.Default()
+	usergroup:=r.Group("/user",AuthMiddleWare())
+
+	r.POST("/auth", authHandler)
+
+}
